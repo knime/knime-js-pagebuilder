0d16b4d86377bd5f385fb96beb8deb607719860a
Thomas Rosenau <thomas.rosenau@knime.com>
2019-09-30 15:53:49 +0200

    AP-12836: Make logging in dev mode configurable
    AP-12836 (Make pagebuilder logging configurable through .env file in dev mode)

--- org.knime.js.pagebuilder/logger.config.js
+++ org.knime.js.pagebuilder/logger.config.js
@@ -1,9 +1,11 @@
+// Development app logger config. Not included in production build.
+
 const { _types } = require('consola');
-let level = process.env.LOG_LEVEL || 'info';
+let level = process.env.VUE_APP_LOG_LEVEL || 'info';
 
 module.exports = {
     level: _types[level].level,
 
     // browser only
-    logToConsole: String(process.env.LOG_TO_CONSOLE) !== 'false'
+    logToConsole: String(process.env.VUE_APP_LOG_TO_CONSOLE) !== 'false'
 };
