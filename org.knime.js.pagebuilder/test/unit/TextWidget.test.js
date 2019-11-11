2c36df12bb332e461e756779d12f05298167f74c
Ben Laney <ben.laney@knime.com>
2019-11-11 15:34:50 +0100

    AP-12919: Added testing for the text output widget
    AP-12919 (Frontend: rewrite text output widget)

---
+++ org.knime.js.pagebuilder/test/unit/TextWidget.test.js
@@ -0,0 +1,137 @@
+import { mount, shallowMount } from '@vue/test-utils';
+
+import TextWidget from '@/components/widgets/output/TextWidget.vue';
+
+describe('TextWidget.vue', () => {
+    let propsData;
+
+    beforeEach(() => {
+        propsData = {
+            nodeConfig: {
+                '@class': 'org.knime.js.core.JSONWebNode',
+                viewValue: {
+                    '@class': 'org.knime.js.base.node.output.text.TextOutputValue'
+                },
+                customCSS: '',
+                viewRepresentation: {
+                    '@class': 'org.knime.js.base.node.output.text.TextOutputRepresentation',
+                    text: 'This is the default text output.',
+                    textFormat: 'Text',
+                    description: 'This is the description',
+                    label: 'This is the label'
+                },
+                initMethodName: 'init',
+                setValidationErrorMethodName: 'setValidationErrorMessage',
+                validateMethodName: 'validate',
+                nodeInfo: {
+                    '@class': 'org.knime.js.core.JSONWebNodeInfo',
+                    nodeName: 'Text Output Widget',
+                    nodeState: 'executed',
+                    nodeAnnotation: '',
+                    nodeErrorMessage: null,
+                    nodeWarnMessage: null,
+                    displayPossible: true
+                },
+                stylesheets: [
+                    '/js-lib/font-awesome/4_7_0/css/font-awesome.min.css',
+                    '/js-lib/knime/service/knime.css',
+                    '/js-lib/jQueryUI/min/themes/base/jquery-ui.min.css',
+                    '/org/knime/js/base/util/quickform/quickformStyles.css'
+                ],
+                namespace: 'org_knime_js_base_node_output_text',
+                javascriptLibraries: [
+                    '/js-lib/knime/service/knime_service_1_0_0.js',
+                    '/js-lib/jQuery/jquery-1.11.0.min.js',
+                    '/js-lib/jQueryUI/min/ui/jquery-ui.min.js',
+                    '/org/knime/js/base/util/quickform/knime_quickform_utils_1_0_0.js',
+                    '/org/knime/js/base/node/output/text/TextOutput.js'
+                ],
+                getViewValueMethodName: 'value'
+            },
+            nodeId: '13:0:12',
+            isValid: true
+        };
+    });
+
+    it('renders', () => {
+        let wrapper = mount(TextWidget, {
+            propsData
+        });
+        expect(wrapper.html()).toBeTruthy();
+        expect(wrapper.isVisible()).toBeTruthy();
+    });
+
+    it('is always valid', () => {
+        let wrapper = mount(TextWidget, {
+            propsData
+        });
+        expect(wrapper.vm.validate()).toBe(true);
+        expect(wrapper.vm.validate(null)).toBe(true);
+        expect(wrapper.vm.validate('')).toBe(true);
+        expect(wrapper.vm.validate(0)).toBe(true);
+        expect(wrapper.vm.validate(NaN)).toBe(true);
+        expect(wrapper.vm.validate(propsData.undefinedProp)).toBe(true);
+    });
+
+    it('recognized the Text element type', () => {
+        let wrapper = mount(TextWidget, {
+            propsData
+        });
+        expect(wrapper.vm.elementType).toBe('p');
+    });
+
+    it('recognized the Preformatted element type', () => {
+        propsData.nodeConfig.viewRepresentation.textFormat = 'Preformatted';
+        let wrapper = mount(TextWidget, {
+            propsData
+        });
+        expect(wrapper.vm.elementType).toBe('pre');
+    });
+
+    it('recognized the HTML element type', () => {
+        propsData.nodeConfig.viewRepresentation.textFormat = 'Html';
+        let wrapper = mount(TextWidget, {
+            propsData
+        });
+        expect(wrapper.vm.elementType).toBe(false);
+    });
+
+    it('has empty error message when valid', () => {
+        let wrapper = shallowMount(TextWidget, {
+            propsData
+        });
+
+        expect(wrapper.vm.errorMessage).toBe('');
+    });
+
+    it('has default error message', () => {
+        propsData.isValid = false;
+        propsData.nodeConfig.viewRepresentation.errorMessage = false;
+        let wrapper = shallowMount(TextWidget, {
+            propsData
+        });
+
+        expect(wrapper.vm.errorMessage).toBe('Current text output value is invalid');
+    });
+
+    it('has warning message', () => {
+        propsData.isValid = false;
+        propsData.nodeConfig.viewRepresentation.errorMessage = false;
+        propsData.nodeConfig.nodeInfo.nodeWarnMessage = 'Testing warning message';
+        let wrapper = shallowMount(TextWidget, {
+            propsData
+        });
+
+        expect(wrapper.vm.errorMessage).toBe('Testing warning message');
+    });
+
+    it('has error message', () => {
+        propsData.isValid = false;
+        propsData.nodeConfig.viewRepresentation.errorMessage = 'Testing error message';
+        let wrapper = shallowMount(TextWidget, {
+            propsData
+        });
+
+        expect(wrapper.vm.errorMessage).toBe('Testing error message');
+    });
+});
