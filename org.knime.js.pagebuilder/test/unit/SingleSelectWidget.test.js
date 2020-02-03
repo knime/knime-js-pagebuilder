0150f4fb85d4790ff0cb474faa992cd42c20a89f
Karl Glatz <karl@glatz.biz>
2020-02-03 10:59:40 +0100

    WEBP-169: add test of ListBox to SingleSelectWidget (WEBP-169: Frontend: rewrite single selection widget (selection))

--- org.knime.js.pagebuilder/test/unit/SingleSelectWidget.test.js
+++ org.knime.js.pagebuilder/test/unit/SingleSelectWidget.test.js
@@ -3,9 +3,10 @@ import { shallowMount } from '@vue/test-utils';
 
 import SingleSelectWidget from '@/components/widgets/input/SingleSelectWidget';
 import RadioButtons from '~/webapps-common/ui/components/forms/RadioButtons';
+import ListBox from '~/webapps-common/ui/components/forms/ListBox';
 
 describe('SingleSelectWidget.vue', () => {
-    let propsDataRadioHorizonal, propsDataRadioVertical;
+    let propsDataRadioHorizonal, propsDataRadioVertical, propsDataDropdown, propsDataList;
 
     beforeEach(() => {
         propsDataRadioHorizonal = {
@@ -145,6 +146,157 @@ describe('SingleSelectWidget.vue', () => {
             nodeId: '4:0:6',
             isValid: false
         };
+
+        propsDataDropdown = {
+            nodeConfig: {
+                '@class': 'org.knime.js.core.JSONWebNode',
+                nodeInfo: {
+                    '@class': 'org.knime.js.core.JSONWebNodeInfo',
+                    nodeErrorMessage: null,
+                    nodeWarnMessage: null,
+                    displayPossible: true,
+                    nodeName: 'Single Selection Widget',
+                    nodeAnnotation: '',
+                    nodeState: 'executed'
+                },
+                stylesheets: [
+                    '/js-lib/font-awesome/4_7_0/css/font-awesome.min.css',
+                    '/js-lib/knime/service/knime.css',
+                    '/js-lib/jQueryUI/min/themes/base/jquery-ui.min.css',
+                    '/org/knime/js/base/util/quickform/quickformStyles.css'
+                ],
+                javascriptLibraries: [
+                    '/js-lib/knime/service/knime_service_1_0_0.js',
+                    '/js-lib/jQuery/jquery-1.11.0.min.js',
+                    '/org/knime/js/base/dialog/selection/single/DropdownSingleSelection.js',
+                    '/org/knime/js/base/dialog/selection/single/ListSingleSelection.js',
+                    '/org/knime/js/base/dialog/selection/single/RadioButtonSingleSelection.js',
+                    '/js-lib/jQueryUI/min/ui/jquery-ui.min.js',
+                    '/org/knime/js/base/util/quickform/knime_quickform_utils_1_0_0.js',
+                    '/org/knime/js/base/node/widget/selection/single/singleSelectionWidget.js'
+                ],
+                initMethodName: 'init',
+                validateMethodName: 'validate',
+                setValidationErrorMethodName: 'setValidationErrorMessage',
+                getViewValueMethodName: 'value',
+                namespace: 'knimeSingleSelectionWidget',
+                viewRepresentation: {
+                    '@class': 'org.knime.js.base.node.base.selection.singleMultiple' +
+                        '.SingleMultipleSelectionNodeRepresentation',
+                    label: 'Dropdown',
+                    description: 'Enter Description',
+                    required: true,
+                    defaultValue: {
+                        '@class': 'org.knime.js.base.node.base.selection.singleMultiple' +
+                            '.SingleMultipleSelectionNodeValue',
+                        value: [
+                            'Option 2'
+                        ]
+                    },
+                    currentValue: {
+                        '@class': 'org.knime.js.base.node.base.selection.singleMultiple' +
+                            '.SingleMultipleSelectionNodeValue',
+                        value: [
+                            'Option 2'
+                        ]
+                    },
+                    possibleChoices: [
+                        'Option 1',
+                        'Option 2',
+                        'Option 3'
+                    ],
+                    type: 'Dropdown',
+                    limitNumberVisOptions: false,
+                    numberVisOptions: 10
+                },
+                viewValue: null,
+                customCSS: ''
+            },
+            nodeId: '4:0:10',
+            isValid: false
+        };
+
+        propsDataList = {
+            nodeConfig: {
+                '@class': 'org.knime.js.core.JSONWebNode',
+                nodeInfo: {
+                    '@class': 'org.knime.js.core.JSONWebNodeInfo',
+                    nodeErrorMessage: null,
+                    nodeWarnMessage: null,
+                    displayPossible: true,
+                    nodeName: 'Single Selection Widget',
+                    nodeAnnotation: '',
+                    nodeState: 'executed'
+                },
+                stylesheets: [
+                    '/js-lib/font-awesome/4_7_0/css/font-awesome.min.css',
+                    '/js-lib/knime/service/knime.css',
+                    '/js-lib/jQueryUI/min/themes/base/jquery-ui.min.css',
+                    '/org/knime/js/base/util/quickform/quickformStyles.css'
+                ],
+                javascriptLibraries: [
+                    '/js-lib/knime/service/knime_service_1_0_0.js',
+                    '/js-lib/jQuery/jquery-1.11.0.min.js',
+                    '/org/knime/js/base/dialog/selection/single/DropdownSingleSelection.js',
+                    '/org/knime/js/base/dialog/selection/single/ListSingleSelection.js',
+                    '/org/knime/js/base/dialog/selection/single/RadioButtonSingleSelection.js',
+                    '/js-lib/jQueryUI/min/ui/jquery-ui.min.js',
+                    '/org/knime/js/base/util/quickform/knime_quickform_utils_1_0_0.js',
+                    '/org/knime/js/base/node/widget/selection/single/singleSelectionWidget.js'
+                ],
+                initMethodName: 'init',
+                validateMethodName: 'validate',
+                setValidationErrorMethodName: 'setValidationErrorMessage',
+                getViewValueMethodName: 'value',
+                namespace: 'knimeSingleSelectionWidget',
+                viewRepresentation: {
+                    '@class': 'org.knime.js.base.node.base.selection.singleMultiple.' +
+                        'SingleMultipleSelectionNodeRepresentation',
+                    label: 'List',
+                    description: 'Enter Description\t',
+                    required: true,
+                    defaultValue: {
+                        '@class': 'org.knime.js.base.node.base.selection.singleMultiple.' +
+                            'SingleMultipleSelectionNodeValue',
+                        value: [
+                            'List Item 3'
+                        ]
+                    },
+                    currentValue: {
+                        '@class': 'org.knime.js.base.node.base.selection.singleMultiple.' +
+                            'SingleMultipleSelectionNodeValue',
+                        value: [
+                            'List Item 3'
+                        ]
+                    },
+                    possibleChoices: [
+                        'List Item 1',
+                        'List Item 2',
+                        'List Item 3',
+                        'List Item 4',
+                        'List Item 5',
+                        'List Item 6',
+                        'List Item 7',
+                        'List Item 8',
+                        'List Item 9',
+                        'List Item 10',
+                        'List Item 11',
+                        'List Item 12',
+                        'List Item 13',
+                        'List Item 14',
+                        'List Item 15',
+                        'List Item 16'
+                    ],
+                    type: 'List',
+                    limitNumberVisOptions: true,
+                    numberVisOptions: 5
+                },
+                viewValue: null,
+                customCSS: ''
+            },
+            nodeId: '4:0:9',
+            isValid: false
+        };
     });
 
     it('renders', () => {
@@ -180,6 +332,22 @@ describe('SingleSelectWidget.vue', () => {
         let rb = wrapper.find(RadioButtons);
         expect(rb).toBeTruthy();
         expect(rb.props('alignment')).toBe('vertical');
+    });
+
+    it('renders list component', () => {
+        propsDataList.isValid = true;
+        let wrapper = shallowMount(SingleSelectWidget, {
+            propsData: propsDataList
+        });
+
+        let rb = wrapper.find(ListBox);
+        expect(rb).toBeTruthy();
+    });
+
+    it('renders dropdown component', () => {
+        propsDataDropdown.isValid = true;
+
+        // TODO: impl
     });
 
     it('has no error message when valid', () => {
