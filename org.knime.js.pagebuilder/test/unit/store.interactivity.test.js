bca928243919099a632d7e96016dd19c99fb4756
Christian Albrecht <christian.albrecht@knime.com>
2020-02-19 15:56:53 +0100

    WEBP-73: finishing test for selection translators
    WEBP-73 (Support selection translators/handlers)

--- org.knime.js.pagebuilder/test/unit/store.interactivity.test.js
+++ org.knime.js.pagebuilder/test/unit/store.interactivity.test.js
@@ -1,4 +1,4 @@
-/* eslint-disable max-nested-callbacks */
+/* eslint-disable max-lines, max-nested-callbacks */
 import { createLocalVue } from '@vue/test-utils';
 import Vuex from 'vuex';
 
@@ -22,7 +22,7 @@ describe('Interactivity store', () => {
     });
 
     describe('mutations', () => {
-        let subscriberId = 'selection-0.0.9';
+        const subscriberId = 'selection-0.0.9';
 
         it('add subscriber', () => {
             expect(store.state).toEqual({});
@@ -112,9 +112,9 @@ describe('Interactivity store', () => {
 
     describe('actions', () => {
         describe('adding subscribers', () => {
-            let subscriberId = 'selection-0.0.9';
-            let dataId = 'selection-0.0.7';
-            let data = { elements: ['dummyData'] };
+            const subscriberId = 'selection-0.0.9';
+            const dataId = 'selection-0.0.7';
+            const data = { elements: ['dummyData'] };
         
             beforeEach(() => {
                 store.commit('updateData', { id: dataId, data });
@@ -160,8 +160,8 @@ describe('Interactivity store', () => {
 
         });
         describe('removing subscribers', () => {
-            let id = 'selection-12345-12345-12345';
-            let payload = { id, callback: jest.fn(), elementFilter: 2 };
+            const id = 'selection-12345-12345-12345';
+            const payload = { id, callback: jest.fn(), elementFilter: 2 };
             
             it('allows removing a subscriber', () => {
                 store.commit('addSubscriber', payload);
@@ -177,13 +177,13 @@ describe('Interactivity store', () => {
         });
         describe('publish data', () => {
 
-            let publishId = '0.0.7 Bond';
+            const publishId = '0.0.7 Bond';
 
             describe('notifies subscribers of changes', () => {
                 
-                let filterId = '0.0.8 Bill';
-                let filterId2 = '0.0.9 Spectre';
-                let minimalData = { elements: [{ id: filterId, data: 42 }] };
+                const filterId = '0.0.8 Bill';
+                const filterId2 = '0.0.9 Spectre';
+                const minimalData = { elements: [{ id: filterId, data: 42 }] };
                 let subscriberCallback = jest.fn();
                 let filteredCallback = jest.fn();
                 
@@ -258,7 +258,7 @@ describe('Interactivity store', () => {
             });
             describe('handles changesets', () => {
 
-                let universalRow = 'row42';
+                const universalRow = 'row42';
                 let britishRows;
 
                 beforeEach(() => {
@@ -380,7 +380,7 @@ describe('Interactivity store', () => {
             });
             describe('handles regular updates', () => {
 
-                let minimalData = { elements: [{ id: '0.0.7', data: 'wibble' }] };
+                const minimalData = { elements: [{ id: '0.0.7', data: 'wibble' }] };
 
                 it('throws error on invalid payload', () => {
                     let invalidPayload = { id: publishId, data: { wibble: 'foo', wobble: 'bar' } };
@@ -406,14 +406,234 @@ describe('Interactivity store', () => {
                     store.dispatch('publish', payload2);
                     expect(store.state[publishId].data).toEqual(changedData);
                 });
-                it('does not update on unchanged data', () => {
-                    
+                it('does not update on unchanged data element', () => {
+                    let multipleElementData = JSON.parse(JSON.stringify(minimalData));
+                    multipleElementData.elements.push({ id: '0.0.9', data: 'wubble' });
+                    let payload = { id: publishId, data: multipleElementData };
+                    store.dispatch('publish', payload);
+                    expect(store.state[publishId]).toBeDefined();
+                    expect(store.state[publishId].data).toEqual(multipleElementData);
+                    payload = { id: publishId, data: minimalData };
+                    store.dispatch('publish', payload);
+                    expect(store.state[publishId].data).toEqual(multipleElementData);
                 });
             });
         });
-        describe('handles selection translators', () => {
-            it('allows registering a selection translator', () => {
-                // TODO with WEBP-73
+        describe('selection translators', () => {
+            
+            const prefix = 'selection-';
+            const sourceID = '0.0.7';
+            const targetIDs = ['0.0.9'];
+            const multipleTargetIDs = ['0.0.3', '0.0.8', '0.0.9'];
+            const translatorId = 42;
+
+            describe('register translators', () => {
+            
+                it('does not subscribe invalid selection translators', () => {
+                    let invalidTranslator = { dummyElement: 'foo' };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator: invalidTranslator });
+                    expect(store.state).toEqual({});
+                    invalidTranslator = { sourceID };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator: invalidTranslator });
+                    expect(store.state).toEqual({});
+                    invalidTranslator = { targetIDs };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator: invalidTranslator });
+                    expect(store.state).toEqual({});
+                    invalidTranslator = { sourceID, targetIDs };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator: invalidTranslator });
+                    expect(store.state).toEqual({});
+                    invalidTranslator = { sourceID, targetIDs, forward: false };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator: invalidTranslator });
+                    expect(store.state).toEqual({});
+                });
+                it('allows registering a forwarding selection translator without mapping', () => {
+                    let translator = { sourceID, targetIDs, forward: true };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator });
+                    expect(store.state[prefix + sourceID]).toBeDefined();
+                    expect(store.state[prefix + sourceID].subscribers[0]).toBeDefined();
+                    expect(store.state[prefix + targetIDs[0]]).toBeDefined();
+                    expect(store.state[prefix + targetIDs[0]].subscribers[0]).toBeDefined();
+                });
+                it('allows registering a selection translator with mapping', () => {
+                    let dummyMapping = {};
+                    let translator = { sourceID, targetIDs, mapping: dummyMapping };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator });
+                    expect(store.state[prefix + sourceID]).toBeDefined();
+                    expect(store.state[prefix + sourceID].subscribers[0]).toBeDefined();
+                    expect(store.state[prefix + targetIDs[0]]).toBeDefined();
+                    expect(store.state[prefix + targetIDs[0]].subscribers[0]).toBeDefined();
+                });
+                it('registers multiple targets of a selection translator', () => {
+                    let translator = { sourceID, targetIDs: multipleTargetIDs, forward: true };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator });
+                    expect(store.state[prefix + sourceID]).toBeDefined();
+                    expect(store.state[prefix + sourceID].subscribers[0]).toBeDefined();
+                    multipleTargetIDs.forEach((targetID) => {
+                        expect(store.state[prefix + targetID]).toBeDefined();
+                        expect(store.state[prefix + targetID].subscribers[0]).toBeDefined();
+                    });
+                });
+            });
+            describe('selection event mapping', () => {
+                it('forwards selection event to target', () => {
+                    let translator = { sourceID, targetIDs, forward: true };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator });
+                    let subscriber = jest.fn();
+                    store.dispatch('subscribe', { id: prefix + targetIDs[0], callback: subscriber });
+                    expect(subscriber).not.toHaveBeenCalled();
+                    let payload = { changeSet: { added: ['Row42'] } };
+                    store.dispatch('publish', { id: prefix + sourceID, data: payload });
+                    expect(subscriber).toHaveBeenCalledWith(prefix + targetIDs[0], expect.objectContaining(payload));
+                });
+                it('forwards selection event to multiple targets', () => {
+                    let translator = { sourceID, targetIDs: multipleTargetIDs, forward: true };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator });
+                    let callbacks = [];
+                    multipleTargetIDs.forEach((targetID) => {
+                        let callback = jest.fn();
+                        callbacks.push(callback);
+                        store.dispatch('subscribe', { id: prefix + targetID, callback });
+                        expect(callback).not.toHaveBeenCalled();
+                    });
+                    let payload = { changeSet: { added: ['Row42'] } };
+                    store.dispatch('publish', { id: prefix + sourceID, data: payload });
+                    callbacks.forEach((callback) => {
+                        expect(callback).toHaveBeenCalledWith(expect.anything(), expect.objectContaining(payload));
+                    });
+                });
+                it('forwards selection event to source', () => {
+                    let translator = { sourceID, targetIDs, forward: true };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator });
+                    let subscriber = jest.fn();
+                    store.dispatch('subscribe', { id: prefix + sourceID, callback: subscriber });
+                    expect(subscriber).not.toHaveBeenCalled();
+                    let payload = { changeSet: { added: ['Row42'] } };
+                    store.dispatch('publish', { id: prefix + targetIDs[0], data: payload });
+                    expect(subscriber).toHaveBeenCalledWith(prefix + sourceID, expect.objectContaining(payload));
+                });
+
+                it('does not map selection event without change set', () => {
+                    let dummyMapping = {};
+                    let translator = { sourceID, targetIDs, mapping: dummyMapping };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator });
+                    let payload = { elements: [{ id: 'foo', wibble: 'wobble' }] };
+                    expect(() => store.dispatch('publish', { id: prefix + sourceID, data: payload })).toThrow();
+                });
+                it('maps selection event with mapping to target', () => {
+                    let mapping = { wibble: ['wobble'] };
+                    let translator = { sourceID, targetIDs, mapping };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator });
+                    let subscriber = jest.fn();
+                    store.dispatch('subscribe', { id: prefix + targetIDs[0], callback: subscriber });
+                    let payload = { changeSet: { added: ['wibble'] } };
+                    store.dispatch('publish', { id: prefix + sourceID, data: payload });
+                    expect(subscriber).toHaveBeenCalledWith(prefix + targetIDs[0], expect.objectContaining({
+                        changeSet: { added: mapping.wibble }
+                    }));
+                });
+                it('maps selection event with simple mapping to source', () => {
+                    let mapping = { wibble: ['wobble'] };
+                    let translator = { sourceID, targetIDs, mapping };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator });
+                    let subscriber = jest.fn();
+                    store.dispatch('subscribe', { id: prefix + sourceID, callback: subscriber });
+                    let payload = { changeSet: { added: ['wobble'] } };
+                    store.dispatch('publish', { id: prefix + targetIDs[0], data: payload });
+                    expect(subscriber).toHaveBeenCalledWith(prefix + sourceID, expect.objectContaining({
+                        changeSet: { added: ['wibble'] }
+                    }));
+                });
+                it('does not map selection items not existing in mapping', () => {
+                    let mapping = { wibble: ['wobble'] };
+                    let translator = { sourceID, targetIDs, mapping };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator });
+                    let subscriber = jest.fn();
+                    store.dispatch('subscribe', { id: prefix + targetIDs[0], callback: subscriber });
+                    let payload = { changeSet: { added: ['foo'] } };
+                    store.dispatch('publish', { id: prefix + sourceID, data: payload });
+                    expect(subscriber).not.toHaveBeenCalled();
+                });
+                it('maps selection event with multiple mapping to target', () => {
+                    let mapping = { wibble: ['wobble', 'wubble', 'flob'] };
+                    let translator = { sourceID, targetIDs, mapping };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator });
+                    let subscriber = jest.fn();
+                    store.dispatch('subscribe', { id: prefix + targetIDs[0], callback: subscriber });
+                    let payload = { changeSet: { added: ['wibble'] } };
+                    store.dispatch('publish', { id: prefix + sourceID, data: payload });
+                    expect(subscriber).toHaveBeenCalledWith(prefix + targetIDs[0], expect.objectContaining({
+                        changeSet: { added: mapping.wibble }
+                    }));
+                    payload = { changeSet: { removed: ['wibble'] } };
+                    store.dispatch('publish', { id: prefix + sourceID, data: payload });
+                    expect(subscriber).toHaveBeenLastCalledWith(prefix + targetIDs[0], expect.objectContaining({
+                        changeSet: { removed: mapping.wibble }
+                    }));
+                });
+                it('maps selection event with multiple mapping to source', () => {
+                    let mapping = { wibble: ['wobble', 'wubble', 'flob'] };
+                    let translator = { sourceID, targetIDs, mapping };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator });
+                    let subscriber = jest.fn();
+                    store.dispatch('subscribe', { id: prefix + sourceID, callback: subscriber });
+                    let payload = { changeSet: { added: mapping.wibble } };
+                    store.dispatch('publish', { id: prefix + targetIDs[0], data: payload });
+                    expect(subscriber).toHaveBeenCalledWith(prefix + sourceID, expect.objectContaining({
+                        changeSet: { added: ['wibble'] }
+                    }));
+                    payload = { changeSet: { removed: mapping.wibble } };
+                    store.dispatch('publish', { id: prefix + targetIDs[0], data: payload });
+                    expect(subscriber).toHaveBeenLastCalledWith(prefix + sourceID, expect.objectContaining({
+                        changeSet: { removed: ['wibble'] }
+                    }));
+                });
+                it('handles partial mappings to source', () => {
+                    let mapping = { wibble: ['wobble', 'wubble', 'flob'] };
+                    let translator = { sourceID, targetIDs, mapping };
+                    store.dispatch('registerSelectionTranslator', { translatorId, translator });
+                    let subscriber = jest.fn();
+                    store.dispatch('subscribe', { id: prefix + sourceID, callback: subscriber });
+                    
+                    let payload = { changeSet: { added: ['wobble'] } };
+                    store.dispatch('publish', { id: prefix + targetIDs[0], data: payload });
+                    // adding a single mapped item results in a new partial selection
+                    expect(subscriber).toHaveBeenCalledWith(prefix + sourceID, expect.objectContaining({
+                        changeSet: { partialAdded: ['wibble'] }
+                    }));
+                    
+                    jest.resetAllMocks();
+                    payload = { changeSet: { added: ['wubble'] } };
+                    store.dispatch('publish', { id: prefix + targetIDs[0], data: payload });
+                    // no change to mapped event, subscriber is not called
+                    expect(subscriber).not.toHaveBeenCalled();
+                    
+                    payload = { changeSet: { added: ['flob'] } };
+                    store.dispatch('publish', { id: prefix + targetIDs[0], data: payload });
+                    // all mapped items added which adds the whole item and removes partial
+                    expect(subscriber).toHaveBeenLastCalledWith(prefix + sourceID, expect.objectContaining({
+                        changeSet: { added: ['wibble'], partialRemoved: ['wibble'] }
+                    }));
+
+                    payload = { changeSet: { removed: ['wobble'] } };
+                    store.dispatch('publish', { id: prefix + targetIDs[0], data: payload });
+                    // removing a single mapped item results in a new partial selection
+                    expect(subscriber).toHaveBeenLastCalledWith(prefix + sourceID, expect.objectContaining({
+                        changeSet: { removed: ['wibble'], partialAdded: ['wibble'] }
+                    }));
+
+                    jest.resetAllMocks();
+                    payload = { changeSet: { removed: ['flob'] } };
+                    store.dispatch('publish', { id: prefix + targetIDs[0], data: payload });
+                    // no change to mapped event, subscriber is not called
+                    expect(subscriber).not.toHaveBeenCalled();
+
+                    payload = { changeSet: { removed: ['wubble'] } };
+                    store.dispatch('publish', { id: prefix + targetIDs[0], data: payload });
+                    // removing the last remaining mapped item removes the partial selection
+                    expect(subscriber).toHaveBeenLastCalledWith(prefix + sourceID, expect.objectContaining({
+                        changeSet: { partialRemoved: ['wibble'] }
+                    }));
+                });
             });
         });
 
@@ -458,5 +678,4 @@ describe('Interactivity store', () => {
             });
         });
     });
-
 });
