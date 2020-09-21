6ad3bd3f20839eed8e7fb8afc4f4c3801a419480
Christian Albrecht <christian.albrecht@knime.com>
2020-09-21 10:17:22 +0200

    AP-14035: create local file link in download widget
    AP-14035 (Enable file download widget to work in the AP)

--- org.knime.js.pagebuilder/src/components/widgets/output/FileDownloadWidget.vue
+++ org.knime.js.pagebuilder/src/components/widgets/output/FileDownloadWidget.vue
@@ -38,6 +38,11 @@ export default {
             type: String
         }
     },
+    data() {
+        return {
+            runningInAPWrapper: Boolean(window.KnimePageLoader)
+        };
+    },
     computed: {
         viewRep() {
             return this.nodeConfig.viewRepresentation;
@@ -58,6 +63,9 @@ export default {
             return getFileExtension(this.viewRep.path);
         },
         link() {
+            if (this.runningInAPWrapper) {
+                return `file://${this.viewRep.path}`;
+            }
             let getDownloadLinkFunc = this.$store.getters['api/downloadResourceLink'];
             if (!getDownloadLinkFunc) {
                 return null;
