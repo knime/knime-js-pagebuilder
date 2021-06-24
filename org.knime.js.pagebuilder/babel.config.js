27eb103f48a9e444f999664c3a5004373749e8ab
Daniel Bogenrieder <daniel.bogenrieder@knime.com>
2021-06-24 11:32:41 +0200

    AP-16873: change babel useBuiltIns option
    AP-16873 (Calendar widget (Date&Time Widget) is not working in AP and WebPortal)

--- org.knime.js.pagebuilder/babel.config.js
+++ org.knime.js.pagebuilder/babel.config.js
@@ -3,17 +3,7 @@ module.exports = {
         ['@babel/preset-env',
             {
                 corejs: '3',
-                useBuiltIns: 'usage',
-                targets: {
-                    browsers: [
-                        'edge >= 16',
-                        'safari >= 9',
-                        'firefox >= 57',
-                        'ie >= 11',
-                        'ios >= 9',
-                        'chrome >= 49'
-                    ]
-                }
+                useBuiltIns: 'entry'
             }]
     ],
     exclude: ['**/src/**/injectedScripts']
