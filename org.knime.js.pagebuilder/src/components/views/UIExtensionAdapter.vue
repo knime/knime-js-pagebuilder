83298403abfa792a34e66db41a393cfb7e01436b
Paul Bärnreuther <paul.baernreuther@tngtech.com>
2024-03-22 11:21:34 +0100

    Revert "Rename settingsOnClean prop to initialDataChange"
    This reverts commit 08adf27fe407fc658ecc9232a10da0ff7d7910d5.

--- org.knime.js.pagebuilder/src/components/views/UIExtensionAdapter.vue
+++ org.knime.js.pagebuilder/src/components/views/UIExtensionAdapter.vue
@@ -307,7 +307,7 @@ export default defineComponent({
       :api-layer="apiLayer"
       :resource-location="resourceLocation"
       :extension-config="extensionConfig"
-      :initial-data-change="settingsOnClean"
+      :settings-on-clean="settingsOnClean"
       :is-reporting="isReporting"
       :is-dialog-layout="isDialogLayout"
       :shadow-app-style="{ 'flex-grow': 1 }"
