78ea74bda9cc90387fc7d6c06227f49376edd407
Kai Franze <kai.franze@knime.com>
2025-01-22 16:21:14 +0100

    NXT-2655: Equo Chromium version bump to v128.0.7
    NXT-2655 (Update Equo Chromium to 128.x)

--- org.knime.js.pagebuilder/src/components/ui/DebugButton.vue
+++ org.knime.js.pagebuilder/src/components/ui/DebugButton.vue
@@ -14,36 +14,18 @@ export default {
       required: true,
     },
   },
-  data() {
-    return {
-      debugPage: "",
-    };
-  },
   computed: {
     ...mapState("pagebuilder", ["isDialogLayout"]),
-    // Composed URL to either open the correct debugger or the overview page
-    debugUrl() {
-      return `http://localhost:${this.debugPort}${this.debugPage}`;
-    },
-  },
-  async mounted() {
-    // get all existing debugger instances
-    let debuggerInstances = await fetch(
-      `http://localhost:${this.debugPort}/json/list?t=${new Date().getTime()}`,
-    ).then((response) => response.json());
-
-    // when there is only 1 instance we open it (otherwise the list of all instances is displayed)
-    if (debuggerInstances.length === 1) {
-      this.debugPage = debuggerInstances[0].devtoolsFrontendUrl;
-    }
   },
   methods: {
-    openBrowser(url) {
+    openInspector() {
       if (window.openBrowser) {
-        window.openBrowser(url);
+        window.openBrowser(
+          "https://docs.equo.dev/chromium/128.x/developing/debugging.html",
+        );
       } else {
         // eslint-disable-next-line no-console
-        console.warn("No 'openBrowser'-browser function available");
+        console.warn("No 'open'-browser function available");
       }
     },
   },
@@ -56,7 +38,7 @@ export default {
     target="_blank"
     class="button"
     title="Open developer tools"
-    @click="openBrowser(debugUrl)"
+    @click="openInspector()"
   >
     <InspectorIcon />
   </FunctionButton>
