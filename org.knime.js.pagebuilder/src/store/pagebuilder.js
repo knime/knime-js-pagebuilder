85bbe7f82652c992c45000a3034af5cb483f7cf6
Tobias Kampmann <tobias.kampmann@tngtech.com>
2025-03-21 15:50:03 +0100

    NXT-3402: Introduce onChange behaviour for composite views
    NXT-3402 (OnChange behaviour of embedded component views)
    NXT-3402 ()

--- org.knime.js.pagebuilder/src/store/pagebuilder.js
+++ org.knime.js.pagebuilder/src/store/pagebuilder.js
@@ -20,6 +20,8 @@ export const state = () => ({
 
   reportingContent: {},
   imageGenerationWaiting: [],
+
+  cleanViewValuesState: {},
 });
 
 const isViewLayout = (pageContent) => {
@@ -27,7 +29,28 @@ const isViewLayout = (pageContent) => {
   const nodeTypes = Object.keys(nodeViews).map(
     (key) => nodeViews[key].extensionType,
   );
-  return !["dialog"].some((specialType) => nodeTypes.includes(specialType));
+  return !nodeTypes.includes("dialog");
+};
+
+export const generateUniqueStringFromViewValue = (viewValue) => {
+  const sortKeysRecursive = (obj) => {
+    if (typeof obj !== "object" || obj === null) {
+      return obj;
+    }
+
+    if (Array.isArray(obj)) {
+      return obj.map(sortKeysRecursive);
+    }
+
+    return Object.keys(obj)
+      .sort(String.localeCompare)
+      .reduce((sorted, key) => {
+        sorted[key] = sortKeysRecursive(obj[key]);
+        return sorted;
+      }, {});
+  };
+
+  return JSON.stringify(sortKeysRecursive(viewValue));
 };
 
 export const mutations = {
@@ -189,6 +212,25 @@ export const mutations = {
   addImageGenerationWaiting(state, { nodeId }) {
     state.imageGenerationWaiting.push(nodeId);
   },
+
+  addToCleanViewValuesState(state, { nodeId, value }) {
+    const stringifiedNodeViewValue = generateUniqueStringFromViewValue(value);
+    if (typeof state.cleanViewValuesState[nodeId] === "undefined") {
+      state.cleanViewValuesState[nodeId] = stringifiedNodeViewValue;
+    } else {
+      consola.warn(
+        `Tried to add a new initial node view value for ${nodeId} to the clean state, but it already exists. Existing value: ${state.cleanViewValuesState[nodeId]} New value: ${stringifiedNodeViewValue}. Will overwrite the existing value. This is most probably an implementation error.`,
+      );
+    }
+  },
+
+  removeFromCleanViewValuesState(state, nodeId) {
+    if (typeof state.cleanViewValuesState[nodeId] === "undefined") {
+      consola.warn(`No such clean state for node ${nodeId} found.`);
+    } else {
+      delete state.cleanViewValuesState[nodeId];
+    }
+  },
 };
 
 export const actions = {
@@ -277,23 +319,54 @@ export const actions = {
     commit("setWebNodeLoading", { nodeId, loading });
   },
 
-  addValueGetter({ commit }, { nodeId, valueGetter }) {
+  async addValueGetter({ commit, state }, { nodeId, valueGetter }) {
     consola.trace("PageBuilder: add value getter via action: ", nodeId);
-    commit("addValueGetter", { nodeId, valueGetter });
+    commit("addValueGetter", {
+      nodeId,
+      valueGetter,
+    });
+
+    // The component must finish mounting before we can retrieve the initial value.
+    // The `webNodesLoading` array tracks nodes whose components are still mounting.
+    // We wait in a loop until the node is removed from `webNodesLoading`, indicating
+    // the component is ready. If it takes too long (30 retries = 3 seconds), we fail.
+    // eslint-disable-next-line no-magic-numbers
+    let remainingRetries = 30;
+    while (state.webNodesLoading.includes(nodeId)) {
+      remainingRetries--;
+      if (remainingRetries === 0) {
+        consola.warn(
+          `Could not get initial value for node ${nodeId} after 30 retries`,
+        );
+        return;
+      }
+      await new Promise((resolve) => setTimeout(resolve, 100));
+    }
+    try {
+      const initialValue = await valueGetter();
+      if (initialValue.value) {
+        commit("addToCleanViewValuesState", {
+          nodeId,
+          value: initialValue.value,
+        });
+      }
+    } catch (e) {
+      consola.warn("Could not get initial value for node ", nodeId, e);
+    }
   },
 
   removeValueGetter({ commit }, { nodeId }) {
     consola.trace("PageBuilder: remove value getter via action: ", nodeId);
+    commit("removeFromCleanViewValuesState", nodeId);
     commit("removeValueGetter", nodeId);
   },
 
-  async getViewValues({ state }) {
+  getViewValues({ state }) {
     let valuePromises = Object.values(state.pageValueGetters).map((getter) =>
       getter(),
     );
 
-    // eslint-disable-next-line arrow-body-style
-    let values = await Promise.all(valuePromises)
+    return Promise.all(valuePromises)
       .then((values) => {
         return values.reduce((agg, element) => {
           agg[element.nodeId] = element.value;
@@ -304,8 +377,6 @@ export const actions = {
         consola.error(`Could not retrieve all view values: ${e}`);
         return {};
       });
-
-    return values;
   },
 
   addValidator({ commit }, { nodeId, validator }) {
@@ -318,11 +389,11 @@ export const actions = {
     commit("removeValidator", nodeId);
   },
 
-  async getValidity({ state }) {
+  getValidity({ state }) {
     let validityPromises = Object.values(state.pageValidators).map(
       (validator) => validator(),
     );
-    let validity = await Promise.all(validityPromises)
+    return Promise.all(validityPromises)
       .then((validityArray) =>
         validityArray.reduce((obj, nodeResp) => {
           obj[nodeResp.nodeId] = nodeResp.isValid;
@@ -333,7 +404,6 @@ export const actions = {
         consola.error(`Page validation failed: ${e}`);
         return {};
       });
-    return validity;
   },
 
   addValidationErrorSetter({ commit }, { nodeId, errorSetter }) {
@@ -383,6 +453,90 @@ export const actions = {
       nodesReExecuting,
     );
     commit("setNodesReExecuting", nodesReExecuting);
+  },
+
+  async isDirty({ state }, _) {
+    consola.debug("PageBuilder: checking if page is dirty");
+    const cleanViewValues = state.cleanViewValuesState;
+    if (typeof cleanViewValues === "undefined") {
+      consola.warn("Initial view values undefined");
+      return false;
+    }
+
+    const nodesToCheck = Object.keys(cleanViewValues);
+
+    if (nodesToCheck.length === 0) {
+      consola.warn("No nodes to check. No initial state?");
+      return false;
+    }
+
+    const gettersOfNodesToCheck = Object.entries(state.pageValueGetters)
+      .filter(([nodeId]) => nodesToCheck.includes(nodeId))
+      .reduce((agg, [nodeId, getter]) => {
+        agg[nodeId] = getter;
+        return agg;
+      }, {});
+
+    const allGettersPresent = nodesToCheck.every(
+      (nodeId) => typeof gettersOfNodesToCheck[nodeId] !== "undefined",
+    );
+
+    if (!allGettersPresent) {
+      consola.warn("Not all getters present. Will not check dirtyness.");
+      return false;
+    }
+
+    return (
+      await Promise.all(
+        nodesToCheck.map(async (nodeId) => {
+          const newViewValue = await gettersOfNodesToCheck[nodeId]();
+          const valueToCompare = generateUniqueStringFromViewValue(
+            newViewValue.value,
+          );
+
+          return valueToCompare !== cleanViewValues[nodeId];
+        }),
+      )
+    ).some((isDirty) => isDirty);
+  },
+
+  async resetDirtyState({ commit, state }, { nodeId }) {
+    consola.debug("PageBuilder: resetting dirty state for nodeId: ", nodeId);
+
+    const getter = state.pageValueGetters[nodeId];
+
+    if (!getter) {
+      consola.warn(
+        "No getter or state found for nodeId: ",
+        nodeId,
+        ". Will not reset dirty state.",
+      );
+      return;
+    }
+
+    try {
+      const newViewValue = await getter();
+
+      if (!newViewValue) {
+        consola.warn(
+          "No newViewValue found for nodeId: ",
+          nodeId,
+          " will reset nonetheless.",
+        );
+      }
+
+      commit("removeFromCleanViewValuesState", nodeId);
+      commit("addToCleanViewValuesState", {
+        nodeId,
+        value: newViewValue?.value,
+      });
+    } catch (error) {
+      consola.warn(
+        "Error while resetting dirty state for nodeId: ",
+        nodeId,
+        error,
+      );
+    }
   },
 
   setReportingContent({ state, commit }, { nodeId, reportingContent }) {
